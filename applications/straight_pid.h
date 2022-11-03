82319677d946f873a86a88de7c3132e8bbabd081
Thesky0418 <1009141896@qq.com>
2022-11-03 14:04:31 +0800

    [add straight_pid]

---
+++ applications/straight_pid.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-11-02     10091       the first version
+ */
+#ifndef APPLICATIONS_STRAIGHT_PID_H_
+#define APPLICATIONS_STRAIGHT_PID_H_
+
+#include "rtthread.h"
+#include "rtdevice.h"
+#include "board.h"
+
+#endif /* APPLICATIONS_STRAIGHT_PID_H_ */
